97a27fb5eee1f8a038a0f5698aa9178492b5b43f
Wouter Kursten <wouter@retouw.nl>
2021-01-30 12:30:12 +0100

    30012021

--- use_class.py
+++ use_class.py
@@ -2,16 +2,30 @@ import requests, getpass
 import vmware_horizon
 
 requests.packages.urllib3.disable_warnings()
-url = input("URL\n")
-username = input("Username\n")
-domain = input("Domain\n")
+#url = input("URL\n")
+url="https://pod1cbr1.loft.lab"
+#username = input("Username\n")
+username = "m_wouter"
+#domain = input("Domain\n")
+domain = "loft.lab"
 pw = getpass.getpass()
 
+hvconnectionobj = vmware_horizon.Connection(username = username,domain = domain,password = pw,url = url)
+hvconnectionobj.hv_connect()
+#hvconnection = vmware_horizon.Connection.hv_connect(hvconnectionobj)
+print(hvconnectionobj.url)
 
-at = vmware_horizon.Connection.hv_connect(username=username,password=pw,url=url,domain=domain)
+
+pools = vmware_horizon.Pools(url=hvconnectionobj.url, access_token=hvconnectionobj.access_token)
+results = pools.list_hvpools()
+monitor = vmware_horizon.Monitor(url=hvconnectionobj.url, access_token=hvconnectionobj.access_token)
 
 
-pools = vmware_horizon.Pools.list_hvpools(url=url, access_token=at)
-print(f'The first Desktop pool is {pools[0]["display_name"]}')
+#id = result[0]["id"]
+#print(id)
+print(results)
+result2 = monitor.virtual_centers()
+print(result2)
+#print(f'The first Desktop pool is {pools[0]["display_name"]}')
 
-vmware_horizon.Connection.hv_disconnect(url=url, access_token=at)
+hvconnectionobj.hv_disconnect()
--- vmware_horizon.py
+++ vmware_horizon.py
@@ -1,31 +1,122 @@
 import json, requests, ssl
 
 class Connection:
-    def hv_connect(username, password, domain, url):
+    def __init__(self, username, password, domain, url):
+        self.username = username
+        self.password = password
+        self.domain = domain
+        self.url = url
+        self.access_token = ""
+
+
+    def hv_connect(self):
         headers = {
             'accept': '*/*',
             'Content-Type': 'application/json',
         }
 
-        data = {"domain": domain, "password": password, "username": username}
+        data = {"domain": self.domain, "password": self.password, "username": self.username}
         json_data = json.dumps(data)
 
-        response = requests.post(f'{url}/rest/login', verify=False, headers=headers, data=json_data)
+        response = requests.post(f'{self.url}/rest/login', verify=False, headers=headers, data=json_data)
         data = response.json()
 
-        access_token = {
+        self.access_token = {
             'accept': '*/*',
             'Authorization': 'Bearer ' + data['access_token']
         }
-        return access_token
+        return self
 
-    def hv_disconnect(url, access_token):
-        requests.post(f'{url}/rest/logout', verify=False, headers=access_token)
+    def hv_disconnect(self):
+        requests.post(f'{self.url}/rest/logout', verify=False, headers=self.access_token)
 
 class Pools:
-    def list_hvpools(url,access_token):
-        response = requests.get(f'{url}/rest/inventory/v1/desktop-pools', verify=False,  headers=access_token)
+    def __init__(self, url, access_token):
+        self.url = url
+        self.access_token = access_token
+
+
+    def list_hvpools(self):
+        response = requests.get(f'{self.url}/rest/inventory/v1/desktop-pools', verify=False,  headers=self.access_token)
         return response.json()
 
+class Monitor:
+    def __init__(self, url, access_token):
+        self.url = url
+        self.access_token = access_token
 
+    def ad_domain(self):
+        response = requests.get(f'{self.url}/rest/monitor/ad-domains', verify=False,  headers=self.access_token)
+        return response.json()
 
+    def connection_servers(self):
+        response = requests.get(f'{self.url}/rest/monitor/connection-servers', verify=False,  headers=self.access_token)
+        return response.json()
+
+    def connection_server(self, id):
+        response = requests.get(f'{self.url}/rest/monitor/v1/connection-servers/{id}', verify=False,  headers=self.access_token)  # Only available in Horion 8.0 2006 and newer
+        return response.json()
+
+    def event_database(self):
+        response = requests.get(f'{self.url}/rest/monitor/event-database', verify=False,  headers=self.access_token)
+        return response.json()
+
+    def farms(self):
+        response = requests.get(f'{self.url}/rest/monitor/farms', verify=False,  headers=self.access_token)
+        return response.json()
+
+    def farm(self):
+        response = requests.get(f'{self.url}/rest/monitor/v1/farms/{id}', verify=False,  headers=self.access_token)   # Only available in Horion 8.0 2006 and newer
+        return response.json()
+
+    def gateways(self):
+        response = requests.get(f'{self.url}/rest/monitor/gateways', verify=False,  headers=self.access_token)
+        return response.json()
+
+    def gateway(self, id):
+        response = requests.get(f'{self.url}/rest/monitor/v1/gateways/{id}', verify=False,  headers=self.access_token)    # Only available in Horion 8.0 2006 and newer
+        return response.json()
+
+    def rds_servers(self):
+        response = requests.get(f'{self.url}/rest/monitor/rds-servers', verify=False,  headers=self.access_token)
+        return response.json()
+
+    def rds_server(self, id):
+        response = requests.get(f'{self.url}/rest/monitor/v1/rds-servers/{id}', verify=False,  headers=self.access_token) # Only available in Horion 8.0 2006 and newer
+        return response.json()
+
+    def saml_authenticators(self):
+        response = requests.get(f'{self.url}/rest/monitor/saml-authenticators', verify=False,  headers=self.access_token)
+        return response.json()
+
+    def saml_authenticator(self, id):
+        response = requests.get(f'{self.url}/rest/monitor/v1/saml-authenticators/{id}', verify=False,  headers=self.access_token) # Only available in Horion 8.0 2006 and newer
+        return response.json()
+
+    def view_composers(self):
+        response = requests.get(f'{self.url}/rest/monitor/view-composers', verify=False,  headers=self.access_token)  # Only available in Horion 8.0 2006 and newer
+        return response.json()
+
+    def view_composer(self, vcId):
+        response = requests.get(f'{self.url}/rest/monitor/v1/view-composers/{id}', verify=False,  headers=self.access_token) # Only available in Horion 8.0 2006
+        return response.json()
+
+    def virtual_centers(self):
+        response = requests.get(f'{self.url}/rest/monitor/virtual-centers', verify=False,  headers=self.access_token)
+        return response.json()
+
+    def virtual_center(self, id):
+        response = requests.get(f'{self.url}/rest/monitor/v1/virtual-centers/{id}', verify=False,  headers=self.access_token) # Only available in Horion 8.0 2006 and newer
+        return response.json()
+
+    def remote_pods(self):
+        response = requests.get(f'{self.url}/rest/monitor/v1/pods', verify=False,  headers=self.access_token) # Only available in Horion 7.11 and newer
+        return response.json()
+
+    def remote_pod(self, id):
+        response = requests.get(f'{self.url}/rest/monitor/v1/pods/{id}', verify=False,  headers=self.access_token)    # Only available in Horion 8.0 2006 and newer
+        return response.json()
+
+    def true_sso(self):
+        response = requests.get(f'{self.url}/rest/monitor/v1/true-sso', verify=False,  headers=self.access_token) # Only available in Horion 7.11 and newer
+        return response.json()
